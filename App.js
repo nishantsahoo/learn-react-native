80f1f997041d18c1da76238d32c35cf7860bd5f2
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-08-21 01:20:00 +0530

    ListView Added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, FlatList, ListView, ScrollView, StyleSheet, Text, View } from 'react-native';
 import axios from "axios";
 
 export default class App extends React.Component {
@@ -12,6 +12,10 @@ export default class App extends React.Component {
       .then(res => {
         this.setState({data: res.data});
       });
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.state = {
+        dataSource: ds.cloneWithRows(['row 1', 'row 2','okay','cool']),
+      };
   }
 
   handler()
@@ -43,6 +47,12 @@ export default class App extends React.Component {
             data={currentComponent}
             renderItem={({item}) =>
               <Text>{item.name}</Text>
+            }
+          />
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) =>
+              <Text>{rowData}</Text>
             }
           />
       </View>
